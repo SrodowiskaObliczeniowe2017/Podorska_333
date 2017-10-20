8a768f5c4f10547f11e3f607c1d29ba3d2aa1466
sehta7 <sehta7@gmail.com>
2017-10-20 20:58:14 +0200

    zmiana

--- program.c
+++ program.c
@@ -3,16 +3,18 @@
 
 int main(void){
 
-int input, output, count = 0;
-printf("podaj cenę\n");
+int input = 0, output, count = 0, i;
+printf("podaj cene\n");
 scanf("%d", &input);
-printf("cena: %d\n", input);
-for(int i = 0; i < 1000; i++){
-	while(input > 3^i){
+for(i = 0; i < 1000; i++){
+	if(input > pow(3,i)){
 		input = input - 3^i;
 		count++;
 	}
+	else if(input == 1){
+        output = 1;
+	}
 }
-output = count;
+output = count + 1;
 printf("najmniejsza liczba monet: %d\n", output);
 }
